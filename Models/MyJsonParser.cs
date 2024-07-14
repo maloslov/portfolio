802d8d1670fc0fe0c79f2c91f31d63b327b40410
maloslov <60790985+maloslov@users.noreply.github.com>
2024-07-14 09:17:37 +0300

    Добавьте файлы проекта.

---
+++ Models/MyJsonParser.cs
@@ -0,0 +1,182 @@
+﻿using System.Text;
+
+namespace portfolio.Models
+{
+    public enum MyJsonContentType
+    {
+        None,
+        Object,
+        Array,
+        Content
+    }
+
+    public class MyJsonElement
+    {
+        public string Name = "";
+        public MyJsonContentType ContentType;
+        public MyJsonElement[]? Content;
+    }
+
+    public class MyJsonParser
+    {
+        public MyJsonElement RootElement { get; set; }
+
+        public MyJsonParser(string json)
+        {
+            var a = (json
+                .Replace("\\n","")
+                .Replace("\n","")
+                .Replace("  ","")
+                .Replace("\": ","\":")
+                );
+            a = Format(a);
+            RootElement = Parse(a.Split("\n"), 0);
+            RootElement.Name = "root";
+        }
+
+        public List<string> FindProperty(string prop)
+        {
+            return FindProperty(prop, RootElement);
+        }
+        
+        // RECURSIVE SEARCH
+        private List<string> FindProperty(string prop, MyJsonElement elem)
+        {
+            var list = new List<string>();
+
+            if (elem.Content != null)
+                foreach (var el in elem.Content)
+                {
+                    if(elem.Name.Equals(prop)
+                        && el.ContentType == MyJsonContentType.Content)
+                    {
+                        list.Add(el.Name);
+                    }
+                    else
+                    {
+                        var l2 = FindProperty(prop, el);
+                        if (l2.Count() > 0)
+                            list.AddRange(l2);
+                    }
+                }
+            return list;
+        }
+
+        private string Format(string json)
+        {
+            var res = new StringBuilder();
+            string str;
+            int depth_cnt = 0;
+            char c;
+
+            for (var i = 0; i < json.Length; i++)
+            {
+                c = json[i];
+                str = c.ToString();
+
+                switch (c)
+                {
+                    case ':':
+                        str = (json[i - 1].Equals('\"')
+                            ? "\t"
+                            : c.ToString()
+                            );
+                        break;
+                    case ',':
+                        str = (json[i + 1].Equals('\"') 
+                            || json[i + 1].Equals('{')
+                            ? $"\t{depth_cnt}\n"
+                            : c.ToString()
+                            );
+                        break;
+                    case '{':
+                        depth_cnt++;
+                        str = (json[i - 1 >= 0 ? i - 1 : i].Equals(':')
+                            || json[i + 1].Equals('\"')
+                            ? $"\t{depth_cnt}\n"
+                            : "{"
+                            );
+                        break;
+                    case '}':
+                        str = (json[i + 1 < json.Length ? i + 1 : i].Equals(']') 
+                            || json[i + 1 < json.Length ? i + 1 : i].Equals('}')
+                            ? $"\t{depth_cnt}"
+                            : "}"
+                            );
+                        depth_cnt--;
+                        break;
+
+                }
+                res.Append(str);
+            }
+            return res.ToString().Trim('[').Trim(']');
+        }
+        // RECURSIVE PARSING
+        private MyJsonElement Parse(string[] formatted, int depth)
+        {
+            var res = new MyJsonElement();
+
+            var elements = new List<MyJsonElement>();
+            var list = new List<string>();
+
+            for (var i = 0; i < formatted.Count(); i++)
+            {
+                var items = formatted[i].Split("\t");
+
+                if(items.Count() < 2)
+                    continue;
+
+                if (depth < Convert.ToInt32(items[items.Count() > 2 ? 2 : 1]))
+                {
+                    if (items.Count() > 2)
+                        list.Add(formatted[i]);
+                }
+                else
+                {
+                    if (list.Count() > 0)
+                    {
+                        elements.Add(Parse(list.ToArray(), depth + 1));
+                        list.Clear();
+                    }
+                    else
+                    {
+                        elements.Add(new MyJsonElement
+                        {
+                            Name = items[0].Trim('\"').Length > 0
+                                ? items[0].Trim('\"')
+                                : items[2],
+                            ContentType = MyJsonContentType.Object,
+                            Content = [new MyJsonElement
+                            {
+                                Name = items[1].Trim('\"'),
+                                ContentType = MyJsonContentType.Content
+                            }]
+                        });
+                    }
+                }
+            }
+            if (list.Count() > 0)
+            {
+                elements.Add(Parse(list.ToArray(), depth + 1));
+                list.Clear();
+            }
+
+            if (elements.Count > 1)
+            {
+                res.Content = elements.ToArray();
+                res.ContentType = MyJsonContentType.Array;
+            }
+            else if(elements.Count > 0)
+            {
+                res = elements[0];
+                res.ContentType = MyJsonContentType.Object;
+            }
+            else
+            {
+                res.ContentType = MyJsonContentType.None;
+            }
+            
+            return res;
+        }
+    }
+}
---
+++ Models/Utils.cs
@@ -0,0 +1,138 @@
+﻿using System.Text;
+
+namespace portfolio.Models
+{
+    public struct Article
+    {
+        public string name;
+        public string url;
+        public string desc;
+        public string updated;
+        public string lang;
+        public string readme;
+
+        public string GetDate()
+        {
+            return Convert.ToDateTime(updated).ToString("d");
+        }
+        public string GetUrlNorm()
+        {
+            return url.Replace("api.", "").Replace("/repos", "");
+        }
+
+        public void UpdateReadme(string text)
+        {
+            readme = text;
+        }
+    }
+
+    public class Utils
+    {
+        public static string FormatRepoSummary(Article record)
+        {
+            var new_text = new StringBuilder();
+            new_text.Append("<details>");
+            new_text.Append($"<summary>{record.name}</summary>");
+            new_text.Append($"<p>Language: {record.lang}</p>");
+            new_text.Append($"<p>Description: {record.desc}</p>");
+            new_text.Append($"<p>Updated: {Convert.ToDateTime(record.updated).ToString("D")}</p>");
+            new_text.Append($"<p>Link: {record.url}</p>");
+            new_text.Append("</details>");
+            return new_text.ToString();
+        }
+        public static string FormatRepoContent(Article text)
+        {
+            throw new NotImplementedException();
+
+
+        }
+
+        public static async Task<Article[]> GetAllReposDataAsync(string username, HttpClient client)
+        {
+            var records = new List<Article>();
+
+            var names = new List<string>();
+            var descs = new List<string>();
+            var updated = new List<string>();
+            var lang = new List<string>();
+
+            var parser = new MyJsonParser(
+                await client.GetStringAsync($"https://api.github.com/users/{username}/repos")
+            );
+
+            names = parser.FindProperty("name");
+            descs = parser.FindProperty("description");
+            updated = parser.FindProperty("updated_at");
+            lang = parser.FindProperty("language");
+
+            if (names.Count()
+                % descs.Count()
+                % updated.Count()
+                % lang.Count()
+                == 0)
+            {
+                for (int i = 0; i < names.Count; i++)
+                {
+                    records.Add(new Article
+                    {
+                        name = names[i],
+                        url = $"https://api.github.com/repos/{username}/{names[i]}",
+                        desc = descs[i],
+                        updated = updated[i],
+                        lang = lang[i],
+                        readme = ""
+                    });
+                }
+            }
+
+            records = records.OrderByDescending(x => x.updated).ToList();
+            return records.ToArray();
+        }
+
+        public static async Task<Article[]> GetReadmeAsync(Article[] articles, int idx, HttpClient client)
+        {
+            var readme = "";
+
+            try
+            {
+                readme = Encoding.UTF8.GetString(
+                    Convert.FromBase64String(
+                        new MyJsonParser(
+                                await client.GetStringAsync(
+                                    $"{articles[idx].url}/contents/README.md"
+                                    )
+                            )
+                        .FindProperty("content")[0]
+                        )
+                    );
+
+            }
+            catch { }
+            finally
+            {
+                articles[idx].readme = FormatReadme(readme);
+            }
+            
+            return articles;
+        }
+
+        public static string FormatReadme(string text)
+        {
+            var new_text = new StringBuilder();
+            new_text.Append("<div>");
+            foreach (var line in text.Split("\n"))
+            {
+                if (line.StartsWith("#"))
+                {
+                    new_text.Append($"<h2>{line.Replace("#", "")}</h2>");
+                }
+                else
+                {
+                    new_text.Append($"{line}<br>\n");
+                }
+            }
+            new_text.Append("</div><br/>");
+            return new_text.ToString();
+        }
+    }
+}
